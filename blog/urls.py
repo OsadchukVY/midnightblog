0c69be00dc97d6ab6a7a3bfc53cb44a14728f9a5
Uladzislau Asadchuk <itsmyfamely@gmail.com>
2021-08-06 04:45:39 +0200

    Add statics and post pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+"""post/ means, that after start URL we will get URL started with post and /.
+    <int:pk> means, that Django waits int and transforms it in var - pk.
+     '/' - symbol in end of URL."""
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
